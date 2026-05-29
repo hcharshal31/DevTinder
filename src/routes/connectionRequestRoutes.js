cd0d6c3222e9596dc012a0e731ca973d632adf0f
hcharshal31 <hcharshal31@gmail.com>
2026-05-30 00:41:43 +0530

    Added new connections functionality

--- src/routes/connectionRequestRoutes.js
+++ src/routes/connectionRequestRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { userAuth }= require("../middleware/auth")
-const connectionRequestController  = require("../controllers/connectionRequestController");
+const { userAuth } = require("../middleware/auth");
+const connectionRequestController = require("../controllers/connectionRequestController");
+const addOrRejectConnection = require("../controllers/addOrRejectConnectionController");
 
-router.post("/request/send/:status/:toUserId", userAuth, connectionRequestController);
+router.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  connectionRequestController,
+);
+
+router.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  addOrRejectConnection,
+);
 
 module.exports = router;
